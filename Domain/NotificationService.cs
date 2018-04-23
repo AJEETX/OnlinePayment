ddd9a58678b73c23274dad60418c841050f1277a
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-04-24 04:16:46 +1000

    renamed to Transact view Filename

--- Domain/NotificationService.cs
+++ Domain/NotificationService.cs
@@ -15,9 +15,33 @@ namespace stripe.Domain
     }
     public class NotificationService : INotificationService
     {
+        readonly string pwd = "abcdef";
         public void Notify(MessageData message)
         {
-            //send email
+            string smtpAddress = "smtp.xxx.com";
+            int portNumber = 587;
+            bool enableSSL = true;
+
+            string emailFrom = message.EmailFrom;
+            string password = pwd;
+            string emailTo = message.EmailTo;
+            string subject = "Hello " + message.CustomerName;
+            string body = "Transaction Successful!";
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFrom);
+                mail.To.Add(emailTo);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Credentials = new NetworkCredential(emailFrom, password);
+                    smtp.EnableSsl = enableSSL;
+                    //smtp.Send(mail);
+                }
+            }
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,8 @@ namespace stripe
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //Configuration.GetConnectionString("local");
+
             StripeConfiguration.SetApiKey(Configuration.GetSection("Stripe")["SecretKey"]);
             if (env.IsDevelopment())
             {
